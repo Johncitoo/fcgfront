3c40456fc17d75ad92056d2727540ebb2073450d
Johncitoo <juan.contreras03@alumnos.ucn.cl>
2025-12-11 07:21:08 -0300

    fix: Remover import de React no usado

--- src/pages/applicant/MilestoneFormPage.tsx
+++ src/pages/applicant/MilestoneFormPage.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 import { useParams, useSearchParams, useNavigate, Link } from 'react-router-dom'
 import RutInput from '../../components/RutInput'
 import FileUpload from '../../components/FileUpload'
